71f261ecbb3c9cff7dee134c58b4dd685a0fb19a
Mauricio Contreras <139881015+mauricio-contreras@users.noreply.github.com>
2025-07-19 16:44:00 +0200

    feat: integrate Redux for state management and apply global styles
    Wrapped the App component with Redux Provider to enable state management using the newly created store. Also included GlobalStyles for consistent theming across the application.
    
    refs: state management integration

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Provider } from 'react-redux'
+import { store } from './redux/store.ts'
+import { GlobalStyles } from './styles/GlobalStyles.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <GlobalStyles />
+      <App />
+    </Provider>
   </StrictMode>,
 )
